8099200fdf29f1fc15cc21d5ad274c2971e8f5f0
Saivelin <vlad.pochkalov@mail.ru>
2023-05-29 20:10:08 +0300

    circle animation

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import { themeState, user } from "../../state/atoms"
 
 const Header = () => {
     const { pathname } = useRouter();
+    const router = useRouter()
     const [theme, setTheme] = useRecoilState(themeState);
     const [userState, setUserState] = useRecoilState(user);
 
@@ -21,6 +22,21 @@ const Header = () => {
             console.log("theme error")
         }
     }
+
+    const draw = {
+        hidden: { pathLength: 0, opacity: 0 },
+        visible: (i) => {
+            const delay = 0;//1 + i * 0.5;
+            return {
+                pathLength: 1,
+                opacity: 1,
+                transition: {
+                    pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+                    opacity: { delay, duration: 0.01 }
+                }
+            };
+        }
+    };
 
     return (
         <header className="header">
@@ -45,9 +61,30 @@ const Header = () => {
                             <button>Войти</button>
                         </Link>
                         :
-                        <Link href={`/users/${userState.id}`} className={pathname.includes("/users") ? "header__nav-active" : "header__nav-hover"}>
-                            <img src="/personal.webp" alt="Личный кабинет" />
-                        </Link>
+                        <>
+                            <motion.svg
+                                onClick={() => { router.push(`/users/${userState.id}`) }}
+                                width="75"
+                                height="75"
+                                viewBox="0 0 100 100"
+                                initial="hidden"
+                                whileHover="visible"
+                                animate={pathname.includes("/users") ? "visible" : ""}
+                                style={{ position: "absolute", marginLeft: "5.025%", cursor: "pointer" }}
+                            >
+                                <motion.circle
+                                    cx="50"
+                                    cy="50"
+                                    r="45"
+                                    stroke="#4069C9"
+                                    variants={draw}
+                                    custom={1}
+                                />
+                            </motion.svg>
+                            <Link href={`/users/${userState.id}`}>
+                                <img src="/personal.webp" alt="Личный кабинет" />
+                            </Link>
+                        </>
                     }
                 </div>
             </nav >
